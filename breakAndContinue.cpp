6696c98efa56882e0f98c6af552c30c9dab0589e
Muhammad-Azeem-Arshad <muhammadazeemarshad0786@gmail.com>
2025-05-05 12:25:59 -0400

    add me

---
+++ breakAndContinue.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i = 0;
+    // while (i < 10)
+    // {
+    //     if (i == 4)
+    //     {
+    //         continue;
+    //     }
+    //     cout << i << "\n";
+    //     i++;
+    // }
+    
+    // while (i < 10)
+    // {
+    //     cout << i << "\n";
+    //     i++;
+    //     if (i == 4)
+    //     {
+    //         continue;
+    //     }
+    // }
+
+    // while (i < 10)
+    // {
+    //     if (i == 4)
+    //     {
+    //         break;
+    //     }
+    //     cout << i << "\n";
+    //     i++;
+    // }
+
+    while (i < 10)
+    {
+        cout<< i << "\n";
+        i++;
+        if (i == 4)
+        {
+            break;
+        }
+        
+    }
+    
+    
+}
