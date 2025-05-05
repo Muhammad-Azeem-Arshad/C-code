e535ea260601f185ac93a67d3a85ab1b9b70b1cc
Muhammad-Azeem-Arshad <muhammadazeemarshad0786@gmail.com>
2025-05-05 12:23:17 -0400

    some changes

---
+++ arrayLength.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> numbers = {20,30,50,60,70,90,8,5,6,8,464,6};
+    int getarrayLenght = numbers.size();
+    cout << getarrayLenght << endl;
+    
+}
